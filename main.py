7b7a678b93dee2a97c86f3c9e63c7025c1edd9fa
Jusxin <72774910+Jusxin@users.noreply.github.com>
2022-01-24 11:23:55 -0400

    added line to see win pattern - mid functional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from threading import currentThread
 import time
+from numpy import square
 import pygame
 from pygame.locals import *
 pygame.init()
@@ -17,6 +19,8 @@ pygame.display.set_caption("TIC TAC TOE")
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 '''Pygame mouse detection'''
 score_font = pygame.font.SysFont('comicsans', 45)
+current_time = 0
+event_timer = 0
 x_o_font = pygame.font.Font('PressStart2P-Regular.ttf', 140)
 squareNet = [pygame.Rect(60, 20, 160, 160), pygame.Rect(220, 20, 160, 160), pygame.Rect(380, 20, 160, 160), pygame.Rect(60, 180, 160, 160), pygame.Rect(
     220, 180, 160, 160), pygame.Rect(380, 180, 160, 160), pygame.Rect(60, 340, 160, 160), pygame.Rect(220, 340, 160, 160), pygame.Rect(380, 340, 160, 160)]
@@ -26,6 +30,8 @@ x_score = 0
 o_score = 0
 x_turn = 0
 o_turn = 0
+flag_win = False
+won = False
 
 
 def emptyGameBoard():
@@ -69,23 +75,24 @@ def drawO():
 
 
 def checkWin():
-    global x_score, o_score
+    global x_score, o_score, event_timer, current_time, flag_win, won
+
     for horizontal in range(0, len(gameBoard), 3):
         trial = list(gameBoard.values())[horizontal:horizontal+3]
         if trial.count('X') == 3 or trial.count('O') == 3:
+            flag_win = True
             if trial[0] == 'X':
                 x_score += 1
-                time.sleep(.1)
                 emptyGameBoard()
             else:
                 o_score += 1
-                time.sleep(.1)
                 emptyGameBoard()
 
     for vertical in range(0, 3):
         vals = list(gameBoard.values())
         trial = [vals[vertical], vals[vertical+3], vals[vertical+6]]
         if trial.count('X') == 3 or trial.count('O') == 3:
+            flag_win = True
             if trial[0] == 'X':
                 x_score += 1
                 start_pos = (squareNet[vertical].x, squareNet[vertical].y)
@@ -94,34 +101,19 @@ def checkWin():
                 emptyGameBoard()
             else:
                 o_score += 1
-                time.sleep(.1)
                 emptyGameBoard()
 
     diagonal = [gameBoard[0], gameBoard[4], gameBoard[8]]
     if diagonal.count('X') == 3 or diagonal.count('O') == 3:
-        if diagonal[0] == 'X':
-            pygame.draw.line(
-                WIN, WHITE, (squareNet[0].x, squareNet[0].y), (squareNet[8].x+160, squareNet[8].y+160), 4)
-            x_score += 1
-            pygame.time.delay(100)
-            emptyGameBoard()
-        else:
-            o_score += 1
-            time.sleep(.1)
-            emptyGameBoard()
+        flag_win = True
+        won = True
+        return squareNet[0], squareNet[8], diagonal[0]
 
     reverse_diagonal = [gameBoard[2], gameBoard[4], gameBoard[6]]
     if reverse_diagonal.count('X') == 3 or reverse_diagonal.count('O') == 3:
-        if reverse_diagonal[0] == 'X':
-            x_score += 1
-            pygame.draw.line(
-                WIN, WHITE, (squareNet[2].x+160, squareNet[2].y), (squareNet[6].x, squareNet[6].y+160), 2)
-            time.sleep(2)
-            emptyGameBoard()
-        else:
-            o_score += 1
-            time.sleep(.1)
-            emptyGameBoard()
+        flag_win = True
+        won = True
+        return squareNet[2], squareNet[6], reverse_diagonal[0]
 
 
 def AI():
@@ -152,7 +144,7 @@ def horizontalCheck():
     global x_turn, o_turn
     for i in range(0, len(gameBoard), 3):
         hor = {i: gameBoard[i], i+1: gameBoard[i+1], i+2: gameBoard[i+2]}
-        if getXCount(hor) > 1 and getOCount(hor) < 1 and (x_turn-o_turn == 1):
+        if getXCount(hor) > 1 and getOCount(hor) < 1 and (x_turn-o_turn == 1) and not flag_win:
             gameBoard[chosenSpot(hor)] = 'O'
             o_turn += 1
 
@@ -161,7 +153,7 @@ def verticalCheck():
     global x_turn, o_turn
     for i in range(0, 3):
         vert = {i: gameBoard[i], i+3: gameBoard[i+3], i+6: gameBoard[i+6]}
-        if getXCount(vert) > 1 and getOCount(vert) < 1 and (x_turn-o_turn == 1):
+        if getXCount(vert) > 1 and getOCount(vert) < 1 and (x_turn-o_turn == 1) and not flag_win:
             gameBoard[chosenSpot(vert)] = 'O'
             o_turn += 1
 
@@ -169,7 +161,7 @@ def verticalCheck():
 def diagonalCheck():
     global x_turn, o_turn
     diag = {0: gameBoard[0], 4: gameBoard[4], 8: gameBoard[8]}
-    if getXCount(diag) > 1 and getOCount(diag) < 1 and (x_turn-o_turn == 1):
+    if getXCount(diag) > 1 and getOCount(diag) < 1 and (x_turn-o_turn == 1) and not flag_win:
         gameBoard[chosenSpot(diag)] = 'O'
         o_turn += 1
 
@@ -177,7 +169,7 @@ def diagonalCheck():
 def reverseDiagonalCheck():
     global x_turn, o_turn
     rev_diag = {2: gameBoard[2], 4: gameBoard[4], 6: gameBoard[6]}
-    if getXCount(rev_diag) > 1 and getOCount(rev_diag) < 1 and (x_turn-o_turn == 1):
+    if getXCount(rev_diag) > 1 and getOCount(rev_diag) < 1 and (x_turn-o_turn == 1) and not flag_win:
         gameBoard[chosenSpot(rev_diag)] = 'O'
         o_turn += 1
 
@@ -211,6 +203,25 @@ while running:
 
     drawX()
     drawO()
+    winning_side = ''
+    if won:
+        winning_side = checkWin()[2]
+        pygame.draw.line(
+            WIN, WHITE, (checkWin()[0].x, checkWin()[0].y), (checkWin()[1].x+160, checkWin()[1].y+160), 4)
+        event_timer = pygame.time.get_ticks()
+        won = False
+    # current_time = pygame.time.get_ticks()
+    if event_timer > 5000:
+        flag_win = False
+        emptyGameBoard()
+        if winning_side == 'X':
+            x_score += 1
+        else:
+            o_score += 1
+        event_timer = 0
+
+    print(current_time, event_timer)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
